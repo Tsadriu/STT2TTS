7a891f06dd30ef51472c54eec178495d22834d81
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2023-02-01 12:50:42 +0100

    Update index.js
    Aggiornato il metodo per il recupero delle lingue dal JSON.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 fetch("languages.json")
-  .then((response) => response.json())
+  .then((response) => {
+    console.log(response);
+    return response.json();
+  })
   .then((languages) => {
+    console.log(languages);
     const languageSelect = document.querySelector("#languageSelect");
+
+    // Remove the default option
+    languageSelect.innerHTML = "";
 
     languages.forEach((language) => {
       const option = document.createElement("option");
