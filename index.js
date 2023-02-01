04712fdb1426a54607b10d43f9f2ba3aa5b13fed
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2023-02-01 12:44:52 +0100

    Update index.js
    Aggiornato il metodo fetch per vedere se i dati vengono veramente popolati.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-fetch('languages.json')
-.then(response => response.json())
-.then(data => {
-    console.log(data)
-})
+fetch("languages.json")
+  .then((response) => response.json())
+  .then((languages) => {
+    const languageSelect = document.querySelector("#languageSelect");
+
+    languages.forEach((language) => {
+      const option = document.createElement("option");
+      option.value = language.code;
+      option.innerText = language.name;
+      languageSelect.appendChild(option);
+    });
+  });
