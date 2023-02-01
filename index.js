18c8bcd432fde68523f6cfc5b01e426da9e88250
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2023-02-01 12:56:45 +0100

    Update index.js
    Terzo test per vedere se la lista delle lingue viene aggiornata...

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 fetch("languages.json")
-  .then((response) => {
-    console.log(response);
-    return response.json();
-  })
+  .then((response) => response.json())
   .then((languages) => {
-    console.log(languages);
     const languageSelect = document.querySelector("#languageSelect");
-
-    // Remove the default option
-    languageSelect.innerHTML = "";
 
     languages.forEach((language) => {
       const option = document.createElement("option");
