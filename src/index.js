fd13f8e4a7c132bda44f1ed42bd1d3db21f41ac4
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2023-02-08 19:34:05 +0100

    Creazione base backend

--- src/index.js
+++ src/index.js
@@ -2,7 +2,22 @@ const express = require("express");
 const config = require("../config.json")
 const app = express();
 
-let a = 2;
-app.use(express.static("static"))
+app.set('view engine', 'ejs');
 
-app.listen(config.port)
+app.get('/', (req, res) => {
+    res.render('index', {
+        languages: {
+            it: "Italiano",
+            en: "English",
+            es: "Español",
+            de: "Deutsch",
+            fr: "Franzosischsh"
+        }
+    });
+  });
+
+// Fornisci file statici dalla cartella static
+// La prima pagina è index.html, potrebbe contenere HTML
+app.use(express.static("static"));
+
+app.listen(config.port, () => console.log("Listening on port " + config.port));
