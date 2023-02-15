0b9ce618b5f95b0554cc43173a8522681f28a85c
Tsadriu <40073056+Tsadriu@users.noreply.github.com>
2023-02-15 21:11:51 +0100

    Ristrutturato il progetto, utilizzo di EJS per aggiungere le lingue in una lista Abilitato accesso all'API Creato metodi per accedere allAPI per ottenere le lingue e mostrarle nell'elenco
    Co-authored-by: lama137 <lama137@users.noreply.github.com>
    Co-authored-by: Alessandro Proto <alex@alexdevs.me>

--- src/index.js
+++ src/index.js
@@ -1,36 +1,49 @@
 require("dotenv").config();
 const express = require("express");
-const {Translate} = require('@google-cloud/translate').v2;
+const path = require("path");
 const config = require("../config.json")
 const app = express();
+const { Translate } = require('@google-cloud/translate').v2;
 
-const translate = new Translate();
+const translator = new Translate();
 
-async function listLanguages() {
-  const [languages] = await translate.getLanguages();
+let languageList = {}
 
-  languages.forEach(language => console.log(language));
+/**
+ * Traduci il testo text a language da from.
+ * Se from è undefined, la lingua viene automaticamente determinata.
+ */
+async function translate(text, language, from) {
+  let data = await translator.translate(text, {
+    from: from,
+    to: language,
+  });
+  return data[0];
 }
-// waaaaaa
-// waaaaaa
-listLanguages();
+
+/**
+ * Salva in cache la lista delle lingue disponibili in Google Translate.
+ */
+async function cacheLanguages() {
+  [languageList] = await translator.getLanguages();
+}
+
+async function onReady() {
+  console.log("Listening on port " + config.port)
+  await cacheLanguages()
+}
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
-    res.render('index', {
-        languages: {
-            it: "Italiano",
-            en: "English",
-            es: "Español",
-            de: "Deutsch",
-            fr: "Franzosischsh"
-        }
-    });
+  res.render('index', {
+    // [ { code: 'langCode', name: 'langName' }, ... ]
+    languages: languageList
   });
+});
 
 // Fornisci file statici dalla cartella static
 // La prima pagina è index.html, potrebbe contenere HTML
 app.use(express.static("static"));
-
-app.listen(config.port, () => console.log("Listening on port " + config.port));
+app.listen(config.port, onReady);
